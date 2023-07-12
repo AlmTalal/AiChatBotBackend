9050dc141dfc0d4b0f93f092f768f2bd78181705
Talal <alamtalal004@gmail.com>
2023-07-12 11:54:44 -0400

    Added the sendMail template for the Ai for a future SendMail Func

---
+++ AIAgent/AiFunctions/sendMail.js
@@ -0,0 +1,24 @@
+const sendMailFuncAi = {
+  name: "sendMail",
+  description: "Sends a mail to anyone",
+  parameters: {
+    type: "object",
+    properties: {
+      message: {
+        type: "string",
+        description: "the message that is gonna be sent",
+      },
+      mail: {
+        type: "string",
+        description: "The mail of the person that is gonna recieve the message",
+      },
+    },
+    required: ["message", "mail"],
+  },
+};
+
+const sendMail = (text, reciever) => {
+  console.log(text, reciever);
+};
+
+module.exports = { sendMailFuncAi, sendMail };

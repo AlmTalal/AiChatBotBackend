70a5020c32a7c880ec395eb5fe59ef31a49087e4
Talal <alamtalal004@gmail.com>
2023-07-12 14:32:55 -0400

    Setting up the project for consuming from the frontend

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+//routers
+const aiChatBot = require("./routers/aiChatBot");
+
+app.use(express.json());
+app.use(cors());
+app.use("/chatbot", aiChatBot);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`${port}`);
+});
